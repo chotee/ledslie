4a6013d87d07501e9f25546963ef43ebfe17a0fb
chotee <chotee@openended.eu>
2017-11-05 23:12:30 +0100

    More tests and some code improvements because of it.

--- ledslie/processors/sequencer.py
+++ ledslie/processors/sequencer.py
@@ -8,18 +8,15 @@ from threading import Timer
 
 from flask.config import Config
 
-timer = None
-
-config = Config(".")
-
-
 class GenericProcessor(object):
     pass
 
 
 class Sequencer(GenericProcessor):
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
         self.queue = deque()
+        self.timer = None
 
     # The callback for when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, rc):
@@ -35,7 +32,12 @@ class Sequencer(GenericProcessor):
         seq_id, sequence = msgpack.unpackb(mqtt_msg.payload)
         client.publish("ledslie/logs/sequencer", "Incoming %s" % seq_id)
         for image, image_info in sequence:
-            image_duration = image_info[b'duration']
+            if len(image) != self.config.get('DISPLAY_WIDTH') * self.config.get('DISPLAY_HEIGHT'):
+                return
+            try:
+                image_duration = image_info[b'duration']
+            except KeyError:
+                return
             self.queue.append([image_duration, image])
         client.publish("ledslie/logs/sequencer", "Send %s" % seq_id)
         if queue_started_empty and self.queue:
@@ -47,27 +49,27 @@ class Sequencer(GenericProcessor):
         client.publish("ledslie/logs/sequencer", "Published image %s" % crc32(image))
 
     def schedule_image(self, client):
-        global timer
         image_duration, image = self.queue.popleft()
         self.send_image(client, image)
         if self.queue:  # there's still work in the queue
             client.publish("ledslie/logs/sequencer", "Scheduling next image for %dms. %d images in queue" %
                            (image_duration, len(self.queue)))
-            timer = Timer(image_duration/1000.0, self.schedule_image, [client]).start()
+            self.timer = Timer(image_duration/1000.0, self.schedule_image, [client]).start()
         else:
             client.publish("ledslie/logs/sequencer", "Image Queue empty")
 
     def run(self, client):
         client.on_connect = self.on_connect
         client.on_message = self.on_message
-        client.connect(config.get('MQTT_BROKER_URL'),
-                       config.get('MQTT_BROKER_PORT'),
-                       config.get('MQTT_KEEPALIVE'))
+        client.connect(self.config.get('MQTT_BROKER_URL'),
+                       self.config.get('MQTT_BROKER_PORT'),
+                       self.config.get('MQTT_KEEPALIVE'))
         client.loop_forever()
 
 
 if __name__ == '__main__':
+    config = Config('.')
     config.from_object('ledslie.defaults')
     config.from_envvar('LEDSLIE_CONFIG')
     client = mqtt.Client()
-    Sequencer().run(client)
+    Sequencer(config).run(client)
--- ledslie/tests/test_serializer.py
+++ ledslie/tests/test_serializer.py
@@ -1,12 +1,13 @@
 import msgpack
 import pytest
+from flask.config import Config
 from paho.mqtt.client import MQTTMessage
 
 from ledslie.processors.sequencer import Sequencer
 
 class FakeClient(object):
     def __init__(self):
-        self.pubs = []
+        self.pubed_messages = []
 
     def connect(self, host, port, keepalive):
         pass
@@ -15,9 +16,20 @@ class FakeClient(object):
         pass
 
     def publish(self, topic, data):
-        self.pubs.append([topic, data])
+        self.pubed_messages.append([topic, data])
 
     def loop_forever(self):
+        pass
+
+    def assert_message_pubed(self, topic):
+        return [msg for msg in self.pubed_messages if msg[0].startswith(topic)]
+
+
+class FakeTimer(object):
+    def __init__(self, delay, func, *args, **kwargs):
+        pass
+
+    def start(self):
         pass
 
 
@@ -29,7 +41,9 @@ class TestSequencer(object):
 
     @pytest.fixture
     def seq(self, client):
-        seq = Sequencer()
+        self.config = Config('.')
+        self.config.from_object('ledslie.defaults')
+        seq = Sequencer(self.config)
         seq.run(client)
         return seq
 
@@ -49,3 +63,48 @@ class TestSequencer(object):
         sequence = []
         mqtt_msg.payload = msgpack.packb([seq_id, sequence])
         seq.on_message(client, userdata, mqtt_msg)
+        assert len(client.assert_message_pubed('ledslie/frames/1')) == 0
+
+    def test_sequence(self, monkeypatch, seq, client):
+        monkeypatch.setattr("ledslie.processors.sequencer.Timer", FakeTimer)
+        image_size = seq.config.get('DISPLAY_WIDTH') * seq.config.get('DISPLAY_HEIGHT')
+        userdata = None
+        mqtt_msg = MQTTMessage()
+        seq_id = 666
+        sequence = [
+            ['0'*image_size, {'duration': 100}],
+            ['1'*image_size, {'duration': 100}],
+            ['2'*image_size, {'duration': 100}],
+        ]
+        mqtt_msg.payload = msgpack.packb([seq_id, sequence])
+        seq.on_message(client, userdata, mqtt_msg)
+        seq.schedule_image(client)
+        seq.schedule_image(client)
+        assert len(client.assert_message_pubed('ledslie/frames/1')) == 3
+        assert client.assert_message_pubed('ledslie/frames/1') == [
+            ['ledslie/frames/1', b'0'*image_size],
+            ['ledslie/frames/1', b'1'*image_size],
+            ['ledslie/frames/1', b'2'*image_size],
+        ]
+
+    def test_sequence_wrong(self, monkeypatch, seq, client):
+        monkeypatch.setattr("ledslie.processors.sequencer.Timer", FakeTimer)
+        image_size = seq.config.get('DISPLAY_WIDTH') * seq.config.get('DISPLAY_HEIGHT')
+        sequence = [
+            ['666', {'duration': 100}],  # Wrong number of bytes in the image
+        ]
+        userdata = None
+        mqtt_msg = MQTTMessage()
+        seq_id = 666
+        mqtt_msg.payload = msgpack.packb([seq_id, sequence])
+        seq.on_message(client, userdata, mqtt_msg)
+        assert len(client.assert_message_pubed('ledslie/frames/1')) == 0
+
+        sequence = [
+            ['0'*image_size, {}],  # No duration information
+        ]
+        mqtt_msg.payload = msgpack.packb([seq_id, sequence])
+        seq.on_message(client, userdata, mqtt_msg)
+        assert len(client.assert_message_pubed('ledslie/frames/1')) == 0
+        assert len(seq.queue) == 0
+
