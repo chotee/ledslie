37eb322edb02d86f999c14c0d8f40f157a3fb6b7
chotee <chotee@openended.eu>
2017-11-22 22:36:33 +0100

    Created Info content source.

--- ledslie/content/info.py
+++ ledslie/content/info.py
@@ -13,3 +13,60 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+
+from twisted.internet import reactor, task
+from twisted.internet.defer import inlineCallbacks
+
+from ledslie.config import Config
+from ledslie.content.generic import GenericContent, CreateContent
+from ledslie.definitions import LEDSLIE_TOPIC_TYPESETTER_3LINES
+from ledslie.messages import TextTripleLinesLayout
+
+
+class InfoReporter(GenericContent):
+    @inlineCallbacks
+    def connectToBroker(self, protocol):
+        '''
+        Connect to MQTT broker
+        '''
+        self.protocol = protocol
+        self.protocol.onDisconnection = self.onDisconnection
+        self.protocol.setWindowSize(3)
+        self.task = task.LoopingCall(self.publish)
+        self.task.start(5, now=False)
+        try:
+            yield self.protocol.connect(self.__class__.__name__, keepalive=60)
+        except Exception as e:
+            self.log.error("Connecting to {broker} raised {excp!s}",
+                           broker=self.config.get('MQTT_BROKER_CONN_STRING'), excp=e)
+        else:
+            self.log.info("Connected to {broker}", broker=self.config.get('MQTT_BROKER_CONN_STRING'))
+
+    def publish(self):
+        def _logFailure(failure):
+            self.log.debug("reported {message}", message=failure.getErrorMessage())
+            return failure
+
+        def _logAll(*args):
+            self.log.debug("all publishing complete args={args!r}", args=args)
+
+        self.log.debug(" >< Starting one round of publishing >< ")
+        msg = TextTripleLinesLayout()
+        msg.lines = [
+            "Ledslie © GNU-AGPL3 ~ ;-)",
+            "https://wiki.techinc.nl/index.php/Ledslie",
+            "https://github.com/techinc/ledslie",
+        ]
+        msg.duration = 5000
+        msg.program = 'info'
+        d = self.protocol.publish(topic=LEDSLIE_TOPIC_TYPESETTER_3LINES.decode(), qos=1, message=bytearray(bytes(msg)))
+        d.addCallbacks(_logAll, _logFailure)
+        return d
+
+
+if __name__ == '__main__':
+    ns = __file__.split(os.sep)[-1]
+    Config(envvar_silent=False)
+    CreateContent(InfoReporter)
+    reactor.run()
