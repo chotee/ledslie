0c4fcaa0d42fc0a232b20d07e680316ab942c2be
chotee <chotee@openended.eu>
2018-03-01 00:09:20 +0100

    Added GMT information to Midnight

--- ledslie/content/midnight.py
+++ ledslie/content/midnight.py
@@ -20,6 +20,7 @@ from datetime import datetime, timedelta
 from dateutil.tz import gettz, tzlocal
 
 from twisted.internet import reactor
+from twisted.internet.defer import Deferred
 from twisted.logger import Logger
 
 from ledslie.config import Config
@@ -40,7 +41,19 @@ def next_midnight(tz_str=None):
     return midnight_home_time
 
 
+def all_gmts() -> dict:
+    gmts = {}
+    for tzname in pytz.all_timezones:
+        tzname_parts = tzname.split('/')
+        if tzname_parts[0] in ['Etc']:
+            if ( tzname_parts[1].startswith("GMT+") or tzname_parts[1].startswith("GMT-") ):
+                gmt_seconds = midnight_seconds(tzname)
+                gmts[gmt_seconds] = tzname_parts[1]
+    return gmts
+
+
 def all_next_midnights():
+    gmts = all_gmts()
     midnights = []
     for tzname in pytz.all_timezones:
         tzname_parts = tzname.split('/')
@@ -51,7 +64,8 @@ def all_next_midnights():
         if tzname_parts[-1].isupper():
             continue
         midnight_diff_seconds = midnight_seconds(tzname)
-        midnights.append((tzname, midnight_diff_seconds))
+        gmt = gmts.get(midnight_diff_seconds, None)
+        midnights.append((tzname, midnight_diff_seconds, gmt))
     return midnights
 
 
@@ -70,9 +84,9 @@ def create_city_name(tzname):
 
 def create_midnight_groups():
     midnight_groups = {}
-    for tz_name, midnight in all_next_midnights():
+    for tz_name, midnight, gmt in all_next_midnights():
         group = midnight_groups.setdefault(midnight, [])
-        group.append(tz_name)
+        group.append((tz_name, gmt))
     return midnight_groups
 
 
@@ -97,12 +111,11 @@ class MidnightContent(GenericContent):
         seconds_to_wait = (moment - now).total_seconds()
         self.reactor.callLater(seconds_to_wait, self.publishMidnight, offset)
 
-    def publishMidnight(self, midnight_offset):
+    def publishMidnight(self, midnight_offset: float) -> Deferred:
         self.call_on_midnight_offset(datetime.now(tzlocal()), midnight_offset)  # setup the new trigger for tomorrow.
-        tz_names = self.tz_groups[midnight_offset]
-        tz_name = random.choice(tz_names)
-        city = create_city_name(tz_name)
-        self.tz_groups = create_midnight_groups()  # Catch changes due to Daylight saving.
+        msg = self.midnight_message(midnight_offset)
+
+        d = self.publish(topic=LEDSLIE_TOPIC_TYPESETTER_3LINES, message=msg, qos=1)
 
         def _logFailure(failure):
             self.log.debug("reported {message}", message=failure.getErrorMessage())
@@ -111,14 +124,24 @@ class MidnightContent(GenericContent):
         def _logAll(*args):
             self.log.debug("all publishing complete args={args!r}", args=args)
 
+        d.addCallbacks(_logAll, _logFailure)
+        return d
+
+    def midnight_message(self, midnight_offset: float) -> TextTripleLinesLayout:
+        tz_names = self.tz_groups[midnight_offset]
+        tz_name, gmt_name = random.choice(tz_names)
+        city = create_city_name(tz_name)
+        self.tz_groups = create_midnight_groups()  # Catch changes due to Daylight saving.
         msg = TextTripleLinesLayout()
         msg.duration = self.config['MIDNIGHT_DISPLAY_DURATION']
         msg.program = 'midnight'
-        msg.lines = ['Midnight in', city, ""]
+        msg.lines = ['Midnight in', city]
+        if gmt_name:
+            msg.lines.append(gmt_name)
+        else:
+            msg.lines.append("")
         msg.valid_time = self.config["MIDNIGHT_SHOW_VALIDITY"]
-        d = self.publish(topic=LEDSLIE_TOPIC_TYPESETTER_3LINES, message=msg, qos=1)
-        d.addCallbacks(_logAll, _logFailure)
-        return d
+        return msg
 
 
 if __name__ == '__main__':
