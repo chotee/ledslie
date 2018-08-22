6bd53c4f3e4319e55f0af9ad6b13ba8a7edbbe57
chotee <chotee@openended.eu>
2018-08-22 21:57:03 +0200

    Added the spacestate install process.

--- spacestate/run.py
+++ spacestate/run.py
@@ -9,10 +9,8 @@ import requests
 from defaults import MQTT_KEEPALIVE, MQTT_BROKER_PORT, MQTT_BROKER_URL, SPACESTATE_MQTT_TOPIC, SPACESTATE_URL, \
     SPACESTATE_POLL_FREQ
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.info)
 log = logging.getLogger(__name__)
-
-# client = mqtt.Client()
 
 
 def retrieve_state(url):
@@ -29,20 +27,16 @@ def main():
     old_state=None
     log.warning("Starting spacestate.")
     while True:
-        log.info("Loop")
+        log.debug("Loop")
         state = retrieve_state(SPACESTATE_URL)
         if not old_state or old_state != state:
-            log.debug("publishing '%s'", state)
+            log.info("publishing '%s'", state)
             publish.single(topic=SPACESTATE_MQTT_TOPIC, payload=state, qos=2, retain=True,
                            hostname=MQTT_BROKER_URL, port=MQTT_BROKER_PORT)
             old_state = state
             log.debug("Done")
         log.debug("sleeping for %s", SPACESTATE_POLL_FREQ)
         time.sleep(SPACESTATE_POLL_FREQ)
-        # res = client.publish(SPACESTATE_MQTT_TOPIC, payload=state, qos=2, retain=True)
-        # res.wait_for_publish()
-        # log.info("Published %s (%s)", state, res.mid)
-        # client.loop(timeout=SPACESTATE_POLL_FREQ)
 
 
 if __name__ == '__main__':
