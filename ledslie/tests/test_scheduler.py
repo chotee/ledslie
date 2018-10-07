0cc09dbc7d85eb2ed1b602db8378669d31601a8d
chotee <chotee@openended.eu>
2018-10-07 02:09:01 +0200

    Alert programs now directly interrupts running programs.

--- ledslie/tests/test_scheduler.py
+++ ledslie/tests/test_scheduler.py
@@ -4,7 +4,7 @@ import json
 
 import ledslie.processors.scheduler
 from ledslie.config import Config
-from ledslie.definitions import LEDSLIE_TOPIC_SEQUENCES_UNNAMED, LEDSLIE_TOPIC_SEQUENCES_PROGRAMS
+from ledslie.definitions import LEDSLIE_TOPIC_SEQUENCES_UNNAMED, LEDSLIE_TOPIC_SEQUENCES_PROGRAMS, ALERT_PRIO_STRING
 from ledslie.messages import FrameSequence, SerializeFrame, Frame
 from ledslie.processors.scheduler import Scheduler
 from ledslie.tests.fakes import FakeMqttProtocol, FakeLogger, FakeLEDScreen
@@ -44,18 +44,17 @@ class TestScheduler(object):
         sched.onPublish(topic, b"", qos=0, dup=False, retain=False, msgId=0)
         assert sched.catalog.is_empty()
 
-    def _test_sequence(self, sched: Scheduler):
+    def _test_sequence_content(self, image_size, frame_seed):
         sequence_info = {}
+        image_sequence = [ [SerializeFrame(bytearray(seed * image_size)), {'duration': 100}] for seed in frame_seed]
+        return [image_sequence, sequence_info]
+
+    def _test_sequence(self, sched: Scheduler):
         image_size = sched.config.get('DISPLAY_SIZE')
-        image_sequence = [
-            [SerializeFrame(bytearray(b'0' * image_size)), {'duration': 100}],
-            [SerializeFrame(bytearray(b'1' * image_size)), {'duration': 100}],
-            [SerializeFrame(bytearray(b'2' * image_size)), {'duration': 100}],
-        ]
-        payload = json.dumps([image_sequence, sequence_info])
+        payload = json.dumps(self._test_sequence_content(image_size, [b'0', b'1', b'2']))
         return payload.encode()
 
-    def test_send_next_frame(self, sched):
+    def etest_send_next_frame(self, sched):
         image_size = sched.config.get('DISPLAY_SIZE')
         sched.catalog.add_program(None, FrameSequence().load(self._test_sequence(sched)))
         assert 0 == len(sched.led_screen._published_frames)
@@ -72,7 +71,46 @@ class TestScheduler(object):
         assert 3 == len(sched.led_screen._published_frames)
         assert bytearray(b'2222') == sched.led_screen._published_frames[-1].img_data[0:4]
         #
-        sched.send_next_frame()  # End of program!
+        sched.send_next_frame()  # End of program, repeats with first frame.
+        assert 4 == len(sched.led_screen._published_frames)
+        assert bytearray(b'0000') == sched.led_screen._published_frames[-1].img_data[0:4]
+
+    def test_alert_sequence(self, sched):
+        sched.catalog.add_program('test', FrameSequence().load(self._test_sequence(sched)))
+        assert 0 == len(sched.led_screen._published_frames)
+
+        sched.send_next_frame()  # Frame 0 of the original programming
+        assert 1 == len(sched.led_screen._published_frames)
+        assert bytearray(b'0000') == sched.led_screen._published_frames[-1].img_data[0:4]
+
+        # Add an alert program
+        image_size = sched.config.get('DISPLAY_SIZE')
+        seq = self._test_sequence_content(image_size, [b'6', b'7'])
+        seq[1]['prio'] = ALERT_PRIO_STRING
+        seq[1]['alert_count'] = 2  # Show the alert frames 2 times.
+        payload = json.dumps(seq)
+        sched.catalog.add_program('some_alert', FrameSequence().load(payload.encode()))
+
+        sched.send_next_frame()  # Frame 0 of the Alert
+        assert 2 == len(sched.led_screen._published_frames)
+        assert bytearray(b'6666') == sched.led_screen._published_frames[-1].img_data[0:4]
+
+        sched.send_next_frame()  # Frame 1 of the Alert
+        assert 3 == len(sched.led_screen._published_frames)
+        assert bytearray(b'7777') == sched.led_screen._published_frames[-1].img_data[0:4]
+
+        sched.send_next_frame()  # Frame 0 of the Alert
+        assert 4 == len(sched.led_screen._published_frames)
+        assert bytearray(b'6666') == sched.led_screen._published_frames[-1].img_data[0:4]
+
+        sched.send_next_frame()  # Frame 1 of the Alert
+        assert 5 == len(sched.led_screen._published_frames)
+        assert bytearray(b'7777') == sched.led_screen._published_frames[-1].img_data[0:4]
+
+        sched.send_next_frame()  # Frame 0 of the original programming
+        assert 6 == len(sched.led_screen._published_frames)
+        assert bytearray(b'0000') == sched.led_screen._published_frames[-1].img_data[0:4]
+
 
     def test_sequence_wrong(self, sched):
         image_size = sched.config.get('DISPLAY_SIZE')
