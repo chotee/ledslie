a1c297680090262626f5283ef4945054365ad21a
chotee <chotee@openended.eu>
2018-11-09 20:48:59 +0100

    Added parser for weechat logs.

--- ircstats/load.py
+++ ircstats/load.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
+import itertools
 import logging
 from typing import Iterable
 
 from ircstats.database import EventDatabase
 from ircstats.events import IrcEvent
-from ircstats.parsers import IrssiParser
+from ircstats.parsers import IrssiParser, WeechatParser
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__file__.split("/")[-1])
 
-log_fn = "../logs/#techinc.log"
-
+log_fn_irssi = "../logs/#techinc.log"
+log_fn_weechat = '../logs/irc.oftc.#techinc.weechatlog'
 
 def parse_file(parser, fd) -> Iterable[IrcEvent]:
     c = 0
@@ -23,16 +24,21 @@ def parse_file(parser, fd) -> Iterable[IrcEvent]:
         line = fd.readline()
         # if c > 5000:
         #     break
-    log.info("c = %s" % c)
+    log.info("Done c = %s" % c)
 
 
 def main():
     log.info("Start")
     db = EventDatabase("irc.sqlite")
-    parser = IrssiParser()
+    irssi_parser = IrssiParser()
+    weechat_parser = WeechatParser()
     c = 1
-    for irc_event in parse_file(parser, open(log_fn, 'r', errors='replace', encoding='UTF-8')):
+    for irc_event in itertools.chain(
+            parse_file(irssi_parser, open(log_fn_irssi, 'r', errors='replace', encoding='UTF-8')),
+            parse_file(weechat_parser, open(log_fn_weechat, 'r', errors='replace', encoding='UTF-8'))
+    ):
         db.add_event(irc_event)
+        # log.info(irc_event)
         if c % 100 == 0:
             log.info("loaded Row #%d", c)
         c += 1
--- ircstats/parsers.py
+++ ircstats/parsers.py
@@ -1,3 +1,4 @@
+import logging
 import re
 from datetime import datetime
 
@@ -5,20 +6,11 @@ from dateutil import parser as dateparser
 
 from ircstats.events import IrcEvent, IrcChatEvent, IrcPresenceEvent
 
+log = logging.getLogger(__file__.split("/")[-1])
 
-class IrssiParser(object):
+class GenericParser(object):
     def __init__(self):
-        self.last_ts = None
-        self.line_patterns = (
-            (re.compile(r"(?P<time>\d{2}:\d{2}) <(?P<mode> |@)(?P<nick>[^ ]+)> (?P<msg>.*)"), self.chat_line),
-            (re.compile(r"(?P<time>\d{2}:\d{2})  \* (?P<nick>[^ ]+) (?P<msg>.*)"), self.action_line),
-            (re.compile(r"--- Log opened (?P<date>.*)"), self.log_open),
-            (re.compile(r"--- Day changed (?P<date>.*)"), self.day_start),
-            (re.compile(r"(?P<time>\d{2}:\d{2}) -!- (?P<nick>[^ ]+) \[(?P<host>.*)\] has (?P<action>[^ ]+)"),
-             self.presence_event),
-            (re.compile(r"(?P<time>\d{2}:\d{2}) -!- (?P<oldnick>[^ ]+) is now known as (?P<newnick>[^ ]+)"),
-             self.nick_change),
-        )
+        self.line_patterns = ()
         self.target_pattern = re.compile(r"(?P<target>\S+): .*")
 
     def parse_line(self, line) -> IrcEvent:
@@ -30,9 +22,57 @@ class IrssiParser(object):
             if match:
                 res = func(**match.groupdict())
                 break
-        # if match is None:
-        #     log.warning("unmatched: %s" % line)
+        if match is None:
+            log.warning("unmatched: %s" % line)
         return res
+
+
+class WeechatParser(GenericParser):
+    def __init__(self):
+        super().__init__()
+        self.line_patterns = (
+            (re.compile(r"(?P<date>\S+ \S+)\t(?P<mode>@|)(?P<nick>(?!<--|--|-->)[^\t\*]+)\t(?P<msg>.*)"), self.chat_line),
+            (re.compile(r"(?P<date>\S+ \S+)\t \*\t(?P<nick>\S+) (?P<msg>.*)"), self.action_line),
+            (re.compile(r"(?P<date>\S+ \S+)\t(<--|-->)?\t(?P<nick>\S+).*has (?P<action>\S+)"), self.presence_event),
+            (re.compile(r"(?P<date>\S+ \S+)\t--\t(?P<oldnick>\S+) is now known as (?P<newnick>\S+)"), self.nick_change),
+        )
+
+    def chat_line(self, date, nick, mode, msg) -> IrcChatEvent:
+        ts = dateparser.parse(date)
+        match = self.target_pattern.match(msg)
+        if match:
+            target = match.group(1)
+        else:
+            target = None
+        return IrcChatEvent(ts, "msg", nick, msg, target)
+
+    def action_line(self, date, nick, msg) -> IrcChatEvent:
+        ts = dateparser.parse(date)
+        return IrcChatEvent(ts, "action", nick, msg)
+
+    def presence_event(self, date, nick, action) -> IrcPresenceEvent:
+        ts = dateparser.parse(date)
+        return IrcPresenceEvent(ts, nick, action)
+
+    def nick_change(self, date, oldnick, newnick) -> IrcPresenceEvent:
+        ts = dateparser.parse(date)
+        return IrcPresenceEvent(ts, oldnick, "change", new_nick=newnick)
+
+
+class IrssiParser(GenericParser):
+    def __init__(self):
+        super().__init__()
+        self.last_ts = None
+        self.line_patterns = (
+            (re.compile(r"(?P<time>\d{2}:\d{2}) <(?P<mode> |@)(?P<nick>[^ ]+)> (?P<msg>.*)"), self.chat_line),
+            (re.compile(r"(?P<time>\d{2}:\d{2})  \* (?P<nick>[^ ]+) (?P<msg>.*)"), self.action_line),
+            (re.compile(r"--- Log opened (?P<date>.*)"), self.log_open),
+            (re.compile(r"--- Day changed (?P<date>.*)"), self.day_start),
+            (re.compile(r"(?P<time>\d{2}:\d{2}) -!- (?P<nick>[^ ]+) \[(?P<host>.*)\] has (?P<action>[^ ]+)"),
+             self.presence_event),
+            (re.compile(r"(?P<time>\d{2}:\d{2}) -!- (?P<oldnick>[^ ]+) is now known as (?P<newnick>[^ ]+)"),
+             self.nick_change),
+        )
 
     def chat_line(self, time, nick, mode, msg) -> IrcChatEvent:
         ts = self._parse_time(self.last_ts, time)
